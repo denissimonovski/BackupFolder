4e473a7a1f721a7064223478051ce3fd4a1c7fa8
Denis Simonovski <simonovskidenis3@gmail.com>
2018-05-30 11:55:36 +0200

    BackUp a Folder and it's contents (without shortcuts/symlinks)
    Signed-off-by: Denis Simonovski <simonovskidenis3@gmail.com>

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ import (
 func main() {
 	var source, dest string
 	var width int
-	c := make(chan int)
-
+	osBroj := make(chan int)
+	formatString := make(chan string)
 	go func() {
 		if runtime.GOOS == "windows" {
 			komanda, err := exec.Command("systeminfo.exe").Output()
@@ -29,10 +29,16 @@ func main() {
 			reg := regexp.MustCompile(`OS Name: *Microsoft Windows (\d{1,2})`)
 			najdi := reg.FindAllStringSubmatch(string(komanda), -1)
 			broj, _ := strconv.Atoi(najdi[0][1])
-			c <- broj
+			osBroj <- broj
 		} else {
-			c <- 10
+			osBroj <- 10
 		}
+	}()
+	go func() {
+		if terminal.IsTerminal(int(os.Stdout.Fd())) {
+			width, _, _ = terminal.GetSize(int(os.Stdout.Fd()))
+		}
+		formatString <- "%-" + strconv.Itoa(int((width-20)/2)-1) + "s%s%-" + strconv.Itoa(int((width-20)/2)-2) + "s%s"
 	}()
 
 	fmt.Println("Vnesi source folder")
@@ -43,14 +49,12 @@ func main() {
 	source = filepath.Clean(source)
 	dest = filepath.Clean(dest)
 
-	if terminal.IsTerminal(int(os.Stdout.Fd())) {
-		width, _, _ = terminal.GetSize(int(os.Stdout.Fd()))
-	}
 	fmt.Print(strings.Repeat("#", width))
 	fmt.Println("Fajlovi za kopiranje")
 	fmt.Print(strings.Repeat("#", width))
-	osnumber := <-c
-	err := copyDir(source, dest, osnumber)
+	osnumber := <-osBroj
+	format := <-formatString
+	err := copyDir(source, dest, format, osnumber)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -65,13 +69,13 @@ func main() {
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
 
-func copyDir(dirsource, dirdest string, osnumber int) (err error) {
+func copyDir(dirsource, dirdest, format string, osnumber int) (err error) {
 	sourceInfo, err := os.Stat(dirsource)
 	if err != nil {
 		fmt.Println(err)
 	}
 	if !sourceInfo.IsDir() {
-		err = copyFile(dirsource, filepath.Join(dirdest, sourceInfo.Name()), osnumber)
+		err = copyFile(dirsource, filepath.Join(dirdest, sourceInfo.Name()), format, osnumber)
 		return
 	}
 
@@ -93,25 +97,18 @@ func copyDir(dirsource, dirdest string, osnumber int) (err error) {
 		destPateka := filepath.Join(dirdest, file.Name())
 
 		if file.IsDir() {
-			err = copyDir(sourcePateka, destPateka, osnumber)
+			err = copyDir(sourcePateka, destPateka, format, osnumber)
 			if err != nil {
 				return
 			}
 		} else {
-			if runtime.GOOS == "windows" {
-				if strings.HasSuffix(file.Name(), ".lnk") {
-					fmt.Println("Shortcut-ot ", file.Name(), " ne se kopira")
-					continue
-				}
-				err = copyFile(sourcePateka, destPateka, osnumber)
-				if err != nil {
-					return
-				}
-			} else {
-				if file.Mode()&os.ModeSymlink == os.ModeSymlink {
-					fmt.Println("Shortcut-ot ", file.Name(), " ne se kopira")
-					continue
-				}
+			if strings.HasSuffix(file.Name(), ".lnk") || file.Mode()&os.ModeSymlink == os.ModeSymlink {
+				fmt.Println("Shortcut-ot ", file.Name(), " ne se kopira")
+				continue
+			}
+			err = copyFile(sourcePateka, destPateka, format, osnumber)
+			if err != nil {
+				return
 			}
 		}
 	}
@@ -119,9 +116,9 @@ func copyDir(dirsource, dirdest string, osnumber int) (err error) {
 	return
 }
 
-func copyFile(src, dst string, osnumber int) (err error) {
-	var ime []string
-	var folderime string
+func copyFile(src, dst, format string, osnumber int) (err error) {
+	var ime1, ime2 []string
+	var folderIme1, folderIme2 string
 	sourceFile, err := os.Open(src)
 	defer sourceFile.Close()
 	if err != nil {
@@ -139,12 +136,14 @@ func copyFile(src, dst string, osnumber int) (err error) {
 		}
 	}()
 
-	ime = strings.Split(src, "\\")
-	folderime = ime[len(ime)-2] + "\\" + ime[len(ime)-1]
+	ime1 = strings.Split(src, "\\")
+	ime2 = strings.Split(dst, "\\")
+	folderIme1 = ime1[len(ime1)-2] + "\\" + ime1[len(ime1)-1]
+	folderIme2 = ime2[len(ime2)-2] + "\\" + ime2[len(ime2)-1]
 	if osnumber > 7 {
-		fmt.Printf("%-70s %s %-70s %s", folderime, "==>", dst, "Se kopira...")
+		fmt.Printf(format, folderIme1, "==>", folderIme2, "Se kopira...")
 	} else {
-		fmt.Printf("%-59s %s", folderime, "Se kopira...")
+		fmt.Printf("%-59s %s", folderIme1, "Se kopira...")
 	}
 	_, err = io.Copy(destFile, sourceFile)
 	fmt.Printf("%s", "Zavrseno\n")
